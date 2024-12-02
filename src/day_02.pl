7104610314c669abf24adeeb6b1b8a304ea2ef77
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2024-12-02 17:08:44 +0000

    Part 2

--- src/day_02.pl
+++ src/day_02.pl
@@ -42,6 +42,23 @@ report_decreasing([R1, R2|T]) :-
     report_decreasing([R2|T]).
 
 
+% part 2
+part2(Levels, Safe):-
+    findall(X, (
+        member(X, Levels),
+        findall(V, variation(X, V), Variants),
+        member(Variant, Variants),
+        save_report(Variant)
+    ), Reports),
+             
+    sort(Reports, Sorted),
+    length(Sorted, Safe).
+
+variation(Report, Variation) :-
+    Variation = Report;
+    select(_, Report, Variation).
+
+
 % Test cases
 example_input("7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9\n").
 ?-  example_input(S), 
@@ -54,6 +71,11 @@ example_input("7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9\
     phrase(file_content(L), Codes),
     part1(L, 2).
 
+?-  example_input(S),
+    string_codes(S, Codes),
+    phrase(file_content(L), Codes),
+    part2(L, 4).
+
 ?-  string_codes("90 91 93 96 93\n", Codes),
     phrase(file_content(L), Codes),
     L = [[90, 91, 93, 96, 93]].
@@ -61,3 +83,8 @@ example_input("7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9\
 solve_part1(Safe) :-
     phrase_from_file(file_content(Levels), "../inputs/day02"),
     part1(Levels, Safe).
+
+
+solve_part2(Safe) :-
+    phrase_from_file(file_content(Levels), "../inputs/day02"),
+    part2(Levels, Safe).
