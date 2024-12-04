9f184c5dcc0b87162e2a701fecb542f1777d3ea7
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2024-12-04 17:24:50 +0000

    day 4

--- src/day_04.pl
+++ src/day_04.pl
@@ -67,14 +67,47 @@ xmas_d(Map, [(X1, Y1, C1), (X2, Y2, C2), (X3, Y3, C3), (X4, Y4, C4)]) :-
     )),
     xmas(Map, [(X1, Y1, C1), (X2, Y2, C2), (X3, Y3, C3), (X4, Y4, C4)]).
 
+% Part 2
+part2(Map, Sum) :-
+    flatten(Map, Flat),
+    findall(X, mas_x(Flat, X), H), length(H, Sum).
+
+    
+mas(Map, [(X1, Y1, C1), (X2, Y2, C2), (X3, Y3, C3), (X4, Y4, C4), (X5, Y5, C5)]) :-
+    ( 
+        C5 = 'A',
+        ((C1 = 'M', C2 = 'S'); (C1 = 'S', C2 = 'M')),
+        ((C3 = 'M', C4 = 'S'); (C3 = 'S', C4 = 'M'))
+    ),
+    select((X1, Y1, C1), Map, R1),
+    select((X2, Y2, C2), R1, R2),
+    select((X3, Y3, C3), R2, R3),
+    select((X4, Y4, C4), R3, R4),
+    select((X5, Y5, C5), R4, _).
+
+mas_x(Map, [(X1, Y1, C1), (X2, Y2, C2), (X3, Y3, C3), (X4, Y4, C4), (X5, Y5, C5)]) :-
+    (
+        X1 #= X5 - 1, X2 #= X5 + 1, X3 #= X5 - 1, X4 #= X5 + 1,
+        Y1 #= Y5 - 1, Y2 #= Y5 + 1, Y3 #= Y5 + 1, Y4 #= Y5 - 1
+    ),
+    mas(Map, [(X1, Y1, C1), (X2, Y2, C2), (X3, Y3, C3), (X4, Y4, C4), (X5, Y5, C5)]).
+ 
 
 % Tests
 ?- Input = `MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX`,
     phrase(parse(Map), Input),
     part1(Map, 18).
 
+?- Input = `MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX`,
+    phrase(parse(Map), Input),
+    part2(Map, 9).
+
 solve_part1(Count) :-
     phrase_from_file(parse(Map), "inputs/day04"),
-    %write(Map),
-    
+    %write(Map), 
     part1(Map, Count).
+
+solve_part2(Count) :-
+    phrase_from_file(parse(Map), "inputs/day04"),
+    %write(Map), 
+    part2(Map, Count).
