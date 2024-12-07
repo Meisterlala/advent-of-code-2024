3fbda8f41a1a74de9217efe55d6f4954edaa0c71
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2024-12-07 11:25:14 +0000

    no

--- src/day_05.pl
+++ src/day_05.pl
@@ -3,57 +3,52 @@
 :- use_module(library(pio)).
 :- use_module(library(portray_text)).
 :- use_module(library(lists)).
+:- use_module(library(lists)).
 :- dynamic(after/2).
 
 set_portray_text(enabled, X, X).
 
-% DCG
-
-input((Po, Pr))-->
-	page_orderings(Po),
-	blanks,
-	pages(Pr),
-	eos.
-
-page_orderings([(X, Y)|Pos])-->
-	number(X),
-	"|",
-	number(Y),
-	(("\n",
-			page_orderings(Pos))|"").
-
-pages([Pr|Prs])-->
-	page(Pr),
-	(("\n",
-			pages(Prs))|"").
-
-page([N|Ns])-->
-	number(N),
-	((",",
-			page(Ns))|""). 
-
-
-% Part 1
-
-part1((Rules, Pages), X) :-
-	write(Rules), nl,
-	write(Pages), nl,
-	
-	forall(member((X, Y), Rules), assertz(after(X, Y))),
-	convlist(correct_order, Pages, GdPages),
-	sum_mid_pages(GdPages, X).
+part1(Pages, X) :-
+    convlist(correct_order, Pages, GdPages),
+    sum_mid_pages(GdPages, X).
 
 correct_order(Pages, Pages) :-
-	forall(subseq(Pages, [X, Y], _), after(X, Y)).
-    
+    forall(subseq(Pages, [X, Y], _), after(X, Y)).
+
 sum_mid_pages(Pages, X) :-
-	maplist(midpage, Pages, MPs),
-	foldl(plus, MPs, 0, X).
+    maplist(midpage, Pages, MPs),
+    foldl(plus, MPs, 0, X).
 
 midpage(Pages, E) :-
-	length(Pages, N),
-	Mid is (N + 1)/2,
-	nth1(Mid, Pages, E).
+    length(Pages, N),
+    Mid is (N + 1) / 2,
+    nth1(Mid, Pages, E).
+
+% Task 2
+
+part2(Pages, X) :-
+    convlist(bad_order, Pages, BadPages),
+    maplist(fix_order, BadPages, GdPages),
+    sum_mid_pages(GdPages, X).
+
+bad_order(Pages, Pages) :- \+ correct_order(Pages, Pages).
+
+fix_order(P, P) :- correct_order(P, P), !.
+fix_order(P0, P) :-
+    append([Prefix, [X, Y], Suffix], P0),
+    \+ after(X, Y),
+    append([Prefix, [Y, X], Suffix], P1),
+    fix_order(P1, P), !.
+
+
+
+input(Rules, Pages) -->
+    sequence(rule, (blank, \+ blank), Rules),
+    blank, blank,
+    sequence(sequence(integer, `,`), (blank, \+ eos), Pages),
+    blank.
+
+rule(X-Y) --> integer(X), `|`, integer(Y).
 
 % Tests
 
@@ -61,4 +56,4 @@ example("47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n6
 
 
 ?- example(E), string_codes(E, C),
-once(phrase(input(I), C, [])), part1(I, 143).
+phrase(input(Rules, Pages), C, []), assert_after(Rules), part1(Pages, 143).
